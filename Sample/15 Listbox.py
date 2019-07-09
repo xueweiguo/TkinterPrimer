ecdbba15e4ca6f6e06ca8311518a1b9d7470ec40
xwg <weiguo_xue@hotmail.com>
2019-07-09 22:23:02 +0800

    添加新文件

--- Sample/15 Listbox.py
+++ Sample/15 Listbox.py
@@ -4,14 +4,49 @@ from tkinter.font import *
 root = Tk()
 
 # create font
-ftiTimes = Font(family='Times', size=24, weight=BOLD, slant=ITALIC)
+ftiTimes = Font(family='Times', size=24, weight=BOLD)
 
-# create a label to change state.
-label = Label(root,text="ColorLabel",
-       background="#ffffa0",foreground="#ff0000",
-       activebackground="#a0ffa0",activeforeground="#0000ff",
-       font=ftiTimes, height=1, width=20)
-label.grid(row=0, column=0, columnspan=4)
+# create a listbox for demo.
+lb = Listbox(root,
+             activestyle='dotbox',
+             bg="#ffffa0",fg="#ff0000",
+             disabledforeground="#3f3f3f",
+             highlightbackground="#0000ff",
+             font=ftiTimes,
+             height=10, width=20)
+
+# add items
+for i in range(0,20):
+    lb.insert(END, str(i))
+
+# bind event
+lb.bind('<<ListboxSelect>>',
+        lambda:label.config(text=str(lb.curselection())))
+lb.grid(row=0, column=0, columnspan=3, sticky=W+E)
+
+# create a Spinbox to change state.
+st_spin = Spinbox(root,
+                  values=('disabled', 'normal'),
+                  state='readonly',
+                  command=lambda:lb.config(state=st_spin.get()))
+st_spin.grid(row=1, column=0, columnspan=1, sticky=W)
+
+# create a Spinbox to change activestyle.
+as_spin = Spinbox(root,
+                  values=('underline', 'dotbox', 'none'),
+                  state='readonly',
+                  command=lambda:lb.config(activestyle=as_spin.get()))
+as_spin.grid(row=1, column=1, columnspan=1, sticky=W)
+
+# create a Spinbox to change select mode.
+sm_spin = Spinbox(root,
+                  values=('browse', 'single', 'multiple', 'extended'),
+                  state='readonly',
+                  command=lambda:lb.config(selectmode=sm_spin.get()))
+sm_spin.grid(row=1, column=2, columnspan=1, sticky=W)
+
+label=Label(root)
+label.grid(row=2, column=0, columnspan=4)
 
 # start and run mainloop
 root.mainloop()
