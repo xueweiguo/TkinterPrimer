dddd0609d84381d6b1be6e779e2f02c64bcab0af
xwg <weiguo_xue@hotmail.com>
2019-09-11 22:01:32 +0800

    代码调整

--- FileBrowser/RenameDialog.py
+++ FileBrowser/RenameDialog.py
@@ -9,8 +9,6 @@ class RenameDialog(Dialog):
         self.fn_var = StringVar()
         self.fn_var.set(self.fn)
         Dialog.__init__(self, file_list.winfo_toplevel(), 'Rename File')
-        # 更新文件列表
-        file_list.select_node(None)
 
     def create_widgets(self):
         # 构建Frame对象以容纳Label和Entry对象
@@ -25,13 +23,13 @@ class RenameDialog(Dialog):
         btn_frame.grid(row=1, column=0, sticky='e')
         # 通过labmda表达式传递构建按钮控件时的对话框控件，路径和文件名信息
         # 修改后的文件名要在按下【OK】按钮是通过fn_var.get获取。
-        ok_btn = Button(btn_frame, text='OK', command=self.rename)
+        ok_btn = Button(btn_frame, text='OK', command=self.__rename)
         ok_btn.grid(row=0, column=0)
         # 取消按钮直接销毁窗口对象
         cancel_btn = Button(btn_frame, text='Cancel', command=self.destroy)
         cancel_btn.grid(row=0, column=1)
 
-    def rename(self):
+    def __rename(self):
         if self.fn_var.get() == self.fn:
             return
         try:
